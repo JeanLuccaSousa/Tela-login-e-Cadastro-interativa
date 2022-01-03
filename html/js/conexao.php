ea90ae1dcafec8c28dea89c5fff836c38ba19f9d
JeanLuccaSousa <jean_luccalp@hotmail.com>
2022-01-03 18:24:37 -0300

    introduzindo o php no sistema

---
+++ html/js/conexao.php
@@ -0,0 +1,17 @@
+<?php
+
+$usuario = 'root';
+$senha = '';
+$database = 'login';
+$host = 'localhost';
+
+$msqli = new mysqli($usuario, $senha, $database, $host);
+
+if($mysqli->error) {
+    die("Falha ao conectar com o banco de dados: " . $mysqli->error);
+}
+
+
+
+
+?>
